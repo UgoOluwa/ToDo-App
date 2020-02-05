01bca15655b27ea1c8a6302863a2a53dc48cdd5d
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-02-05 23:31:14 +0100

    first ToDo Commit

---
+++ Data/ItemsContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ToDo.Data
+{
+    public class ItemsContext : DbContext
+    {
+    }
+}
