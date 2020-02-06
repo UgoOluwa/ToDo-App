cce12b9cc937c2ebf5622e5f56187000a76379b7
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-02-06 02:02:30 +0100

    Scafolded the Items

--- Data/ItemsContext.cs
+++ Data/ItemsContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDo.Data
 {
     public class ItemsContext : DbContext
     {
+        public ItemsContext(DbContextOptions<ItemsContext> options) : base(options) { }
+        public DbSet<Model.Item> Items { get; set; }
+
+
     }
 }
