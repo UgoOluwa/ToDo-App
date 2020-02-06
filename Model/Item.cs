a30e82a1c1f8120bcdfe3c8caa10b169c682d479
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-02-06 02:18:32 +0100

    Did another Migration and updated the DB

--- Model/Item.cs
+++ Model/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,9 @@ namespace ToDo.Model
     public class Item
     {
         public int Id { get; set; }
+        [StringLength(200, ErrorMessage = "Your name is extremely long")]
         public string Name { get; set; }
-
+        [DataType(DataType.Date, ErrorMessage = "You need to input a valid date")]
         public DateTime Due { get; set; }
     }
 }
